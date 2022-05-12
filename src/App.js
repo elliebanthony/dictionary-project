105f106bcfdb74d8f7bb4f50dddac9e9f97e3864
elliebanthony <elliebanthony@gmail.com>
2022-05-12 16:47:06 -0400

    added compenant dictionary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hi there</p>
+        <p className="btn btn-primary">Hi there</p>
       </header>
+      <Dictionary />
+      <footer> Coded by Ellie Anthony</footer>
     </div>
   );
 }
