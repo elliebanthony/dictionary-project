5d3fb6fa2c7e0cf82b6c5c40c5dd08a3726333b4
elliebanthony <elliebanthony@gmail.com>
2022-05-19 17:19:11 -0400

    new api photos in dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,11 +11,20 @@ export default function Dictionary() {
     console.log(response.data[0]);
     setResults(response.data[0]);
   }
+  function handlePexelResponse(response) {
+    console.log(response);
+  }
   function search(event) {
     event.preventDefault();
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelApiKey =
+      "563492ad6f91700001000001c6f3c0764d23473da19805bab2288fa9";
+    let pexelApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+    let headers = { Authorization: `Bearer ${pexelApiKey}` };
+    axios.get(pexelApiUrl, { headers: headers }).then(handlePexelResponse);
   }
   function handleKeyword(event) {
     setKeyword(event.target.value);
